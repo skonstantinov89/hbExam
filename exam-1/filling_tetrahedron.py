7c2ddb701c4de84436796cc5ea120519dc2a2ba5
skonstantinov89 <jovial@mail.bg>
2015-02-04 23:22:52 +0200

    fixed comment

--- exam-1/filling_tetrahedron.py
+++ exam-1/filling_tetrahedron.py
@@ -2,7 +2,7 @@ import math
 
 def fill_tetrahedron(num):
 	volume = (num**3)/(6*math.sqrt(2))/1000
-	# the formula returns ml**3
+	# the formula returns ml
 	return round(volume,2)
 
 print (fill_tetrahedron(100))
