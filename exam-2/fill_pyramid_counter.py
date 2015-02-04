df1503eabdd12f4df8f3401baae319a8e452d235
Stoyan Konstantinov <jovial@mail.bg>
2015-02-04 22:28:16 +0200

    fixed stupidity

--- exam-2/fill_pyramid_counter.py
+++ exam-2/fill_pyramid_counter.py
@@ -1,39 +1,28 @@
 import math
 
+def fill_tetrahedron(num):
+	volume = (num**3)/(6*math.sqrt(2))/1000
+	# the formula returns ml**3
+	return round(volume,2)
+
+
 def tetrahedron_filled(num, water):
-	# http://en.wikipedia.org/wiki/Tetrahedron#Heron-type_formula_for_the_volume_of_a_tetrahedron
-	heron ={
-	'U': num[0],
-	'V': num[1],
-	'W': num[2],
+	volumes = []
+	for eachEdge in num:
+		volumes.append(fill_tetrahedron(eachEdge))
 
-	'u': num[0],
-	'v': num[1],
-	'w': num[2]
-	}
-
-	z = (heron['W'] - heron['u'] + heron['v']) * (heron['u'] - heron['v'] + heron['W'])
-	
-	Z = (heron['v'] - heron['W'] + heron['u']) * (heron['W'] + heron['u'] + heron['v'])
-
-	y = (heron['V'] - heron['w'] + heron['u']) * (heron['w'] - heron['u'] + heron['V'])
-	
-	Y = (heron['u'] - heron['V'] + heron['w']) * (heron['V'] + heron['w'] + heron['u'])
-	
-	x = (heron['U'] - heron['v'] + heron['w']) * (heron['v'] - heron['w'] + heron['U'])
-	
-	X = (heron['w'] - heron['U'] + heron['v']) * (heron['U'] + heron['v'] + heron['w'])
+	counter = 0
+	isElements = len(volumes)
+	while water > 0 and isElements > 0:
+		if (water - min(volumes)) >= 0:
+			isElements -= 1
+			counter +=1
+			water -= min(volumes)
+			del volumes[volumes.index(min(volumes))]
+		else:
+			isElements -=1
+	return counter
 
 
-	a = math.sqrt(x*Y*Z)
-	b = math.sqrt(y*Z*X)
-	c = math.sqrt(z*X*Y)
-	d = math.sqrt(x*y*z)
 
-	volume = math.sqrt((b+c+d-a)*(a-b+c+d)*(a+b-c+d)*(a+b+c-d))  / (192*heron['u']*heron['v']*heron['w'])
-	print (volume)
-	return volume / water
-
-
-pyramidVolume=int(tetrahedron_filled([120,20,30], 10))
-# print (pyramidVolume)
+print (tetrahedron_filled([120,20,30], 10))
